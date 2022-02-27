faca4875a10030ad5c1735b5fc777c8167656d2b
AbdullahRamadan07 <abdullahramadan351@gmail.com>
2022-02-27 22:11:40 +0200

    new changes

--- app/src/main/java/com/example/timetable/Home_Page.java
+++ app/src/main/java/com/example/timetable/Home_Page.java
@@ -3,12 +3,47 @@ package com.example.timetable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class Home_Page extends AppCompatActivity {
 
+    MaterialButton save;
+    EditText task;
+    ListView listView;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__page);
+        final MyDataBase db = new MyDataBase(this);
+        final ArrayList arrayList =db.getAllData();
+        task = findViewById(R.id.task);
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter(Home_Page.this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        save = findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!task.getText().toString().isEmpty()){
+                    if(db.insertTask(task.getText().toString()))
+                        Toast.makeText(Home_Page.this,"Added Successfully",Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(Home_Page.this,"Added Failed",Toast.LENGTH_LONG).show();
+                }
+                else
+                    task.setError("Enter Task");
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/timetable/MyDataBase.java
+++ app/src/main/java/com/example/timetable/MyDataBase.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class MyDataBase extends SQLiteOpenHelper {
 
     public static final String DBName = "login";
@@ -20,13 +22,14 @@ public class MyDataBase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table users(username TEXT primary key, password TEXT)");
+        db.execSQL("create table users(username TEXT primary key, password TEXT, task TEXT, datetime default current_timestamp)");
         String query = "";
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists users");
+        onCreate(db);
     }
 
     public Boolean EditData(String username, String password){
@@ -38,7 +41,31 @@ public class MyDataBase extends SQLiteOpenHelper {
 
         return res != -1;
     }
-
+    public Boolean insertTask(String task){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("task",task);
+        db.replace(column_Title,null,cv);
+        return true;
+    }
+    public ArrayList getAllData(){
+        SQLiteDatabase db =this.getReadableDatabase();
+        ArrayList<String> arrayList = new ArrayList<String>();
+        Cursor res = db.rawQuery("Select (username || ' : ' || task || ' : ' || time('%d-%m-%Y %H:%M:%S',datetime)) as fullname from " + column_Title,null);
+        res.moveToFirst();
+        while(res.isAfterLast() == false){
+           if((res != null)&&(res.getCount()>0)){
+               arrayList.add(res.getString(res.getColumnIndex("fullname")));
+           }
+            res.moveToNext();
+        }
+        return arrayList;
+    }
+    public boolean delete() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE from " + column_Title);
+        return true;
+    }
     public  Boolean checkUserName(String username){
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery("Select * from users where username = ?", new String[] {username});
@@ -50,8 +77,4 @@ public class MyDataBase extends SQLiteOpenHelper {
         Cursor cursor = db.rawQuery("Select * from users where username = ? and password = ?", new String[]{username,password});
         return cursor.getCount() > 0;
     }
-
-
-
-
 }
